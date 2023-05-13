ef56051d9850ac58275e3232d4e08a408991fc4b
Sean Dinwiddie <seandinwiddie@gmail.com>
2023-05-12 21:59:38 -0700

    adding terminal command

--- App.tsx
+++ App.tsx
@@ -4,17 +4,39 @@ import { Provider } from 'react-redux';
 import Terminal from 'react-bash';
 
 import store from './scripts/store';
-// import modules from './scripts/modules';
 import { modules } from './scripts/modules';
 import styles from './styles';
 
 export default function App() {
   const [currentModule, setCurrentModule] = useState(modules.length > 0 ? modules[0] : {});
 
-  const moduleOptions = {};
-  modules.forEach((module, index) => {
-    moduleOptions[`${index + 1}. ${module.name}`] = module;
-  });
+  const moduleOptions = modules.reduce((options, module, index) => {
+    options[`${index + 1}. ${module.name}`] = module;
+    return options;
+  }, {});
+
+  const listModulesCommand = {
+    command: 'modules',
+    description: 'List all available modules',
+    executor: ({ structure, ...props }) => {
+      const output = moduleOptions.map((module) => module.name).join('\n');
+      return { output, structure, ...props };
+    },
+  };
+
+  const extensions = {
+    listModulesCommand: {
+      ...listModulesCommand,
+    },
+    ...(Array.isArray(currentModule.commands)
+      ? currentModule.commands.reduce((acc, command) => {
+          acc[command.command] = {
+            ...command,
+          };
+          return acc;
+        }, {})
+      : {}),
+  };
 
   return (
     <Provider store={store}>
@@ -24,17 +46,12 @@ export default function App() {
         <Terminal
           prefix={currentModule.name}
           description={currentModule.description}
-          commands={currentModule.commands}
+          extensions={extensions}
           structure={moduleOptions}
           theme={Terminal.Themes.DARK}
           styles={{
             root: {
-              height: '100%',
-            },
-          }}
-          extensions={{
-            sampleModule: {
-              reducer: currentModule.reducer,
+              height: '50%',
             },
           }}
         />
